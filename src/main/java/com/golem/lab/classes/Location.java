e7a8c6de43133f04414ec7f6bbe1a94a76287d28
bugde <bugdeer11@gmail.com>
2024-10-26 20:07:25 +0300

    Init

---
+++ src/main/java/com/golem/lab/classes/Location.java
@@ -0,0 +1,8 @@
+package com.golem.lab.classes;
+
+public class Location {
+    private float x;
+    private long y;
+    private Integer z; //Поле не может быть null
+    private String name; //Длина строки не должна быть больше 658, Поле не может быть null
+}
